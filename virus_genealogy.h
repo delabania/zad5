f02384b2e9feb3a63e83fa29cc95ae0d751f6246
delabania <przem0.b95@gmail.com>
2016-12-12 16:54:05 +0100

    excpetions

--- virus_genealogy.h
+++ virus_genealogy.h
@@ -3,12 +3,56 @@
 
 #include <vector>
 
+// Pomocniczo, zeby sprawdzic czy sie kompiluje
+// Do usuniecia po skonczeniu
+#include <string>
+
+
+class Virus {
+public:
+	typedef typename std::string id_type;
+	Virus(id_type const &_id) : id(_id) {
+	}
+	id_type get_id() const {
+		return id;
+	}
+private:
+	id_type id;
+};
+// koniec pomocniczych deklaracji
+
+/* WAZNE INFO Z TRESCI
+ * wszystkie metody klasy VirusGenealogy powinny gwarantować silną odporność
+   na wyjątki, a tam, gdzie to jest możliwe i pożądane, powinny być no-throw;
+ * wyszukiwanie wirusów powinno być szybsze niż liniowe.
+ * zarządzanie pamięcią powinno być zrealizowane za pomocą sprytnych wskaźników
+   z biblioteki standardowej.
+*/
+
+class VirusNotFound : public std::exception {
+	virtual const char* what() const throw() {
+		return "VirusNotFound";
+	}
+};
+class VirusAlreadyCreated : public std::exception {
+	virtual const char* what() const throw() {
+		return "VirusAlreadyCreated";
+	}
+};
+class TriedToRemoveStemVirus : public std::exception {
+	virtual const char* what() const throw() {
+		return "TriedToRemoveStemVirus";
+	}
+};
 
 template <class Virus>
 class VirusGenealogy {
 	using id_type = typename Virus::id_type;
-	
+
 public:
+	VirusGenealogy(const VirusGenealogy &) = delete;
+	VirusGenealogy & operator=(const VirusGenealogy &) = delete;
+
 	VirusGenealogy(id_type const &stem_id);
 	id_type get_stem_id() const;
 	std::vector<id_type> get_children(id_type const &id) const;
