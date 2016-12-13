9958e04fce643341aeb0710abc44f9f94fe5da98
delabania <przem0.b95@gmail.com>
2016-12-13 18:41:55 +0100

    kolejne metody

--- virus_genealogy.h
+++ virus_genealogy.h
@@ -50,6 +50,9 @@ class TriedToRemoveStemVirus : public std::exception {
 
 
 //@TODO : ktore smart-pointery uzyc w konkretnym przypadku?
+//@TODO : wyjatki - nie ma poki co zadnej odpornosci na wyjatki!
+//@TODO : sprawdzanie czy wirus istnieje i wyrzucanie wyjatku, opakowac w 1 funkcje prywatna
+// 		  zamiast copy-pasty
 template <class Virus>
 class VirusGenealogy {
 	using id_type = typename Virus::id_type;
@@ -70,14 +73,40 @@ public:
 	bool exists(id_type const &id) const {
 		return _all_nodes.find(id) != _all_nodes.end();
 	}
-	
-	std::vector<id_type> get_children(id_type const &id) const;
-	std::vector<id_type> get_parents(id_type const &id) const;
-	Virus& operator[](id_type const &id) const;
-	void create(id_type const &id, id_type const &parent_id);
-	void create(id_type const &id, std::vector<id_type> const &parent_ids);
-	void connect(id_type const &child_id, id_type const &parent_id);
-	void remove(id_type const &id);
+	//@TODO : straszny copypaste z get_parents! Wydzielic do jednej funkcji
+	std::vector<id_type> get_children(id_type const &id) const {
+		// 1. sprawdz czy istnieje wirus o identyfikatorze id, wpp rzuc wyjatek
+		auto it = _all_nodes.find(id);
+		if (it == _all_nodes.end())
+			throw new VirusNotFound();
+		// stworz wektor w ktorym zwrocone zostana id nastepnikow
+		std::vector<id_type> children;
+		// dla kazdego dziecka wezla, na ktory pokazuje iterator it wrzuc do wektora
+		// identyfikator dziecka tego wezla
+		for (auto &node_ptr : it->_children)
+			children.push_back(node_ptr->_virus->_get_id());
+		return children;
+	}
+	std::vector<id_type> get_parents(id_type const &id) const {
+		// analogicznie jak w get_children
+		auto it = _all_nodes.find(id);
+		if (it == _all_nodes.end())
+			throw new VirusNotFound();
+		std::vector<id_type> parents;
+		for (auto &node_ptr : it->_parents)
+			parents.push_back(node_ptr->_virus->_get_id());
+		return parents;
+	}
+	Virus& operator[](id_type const & id) const {
+		auto it = _all_nodes.find(id);
+		if (it == _all_nodes.end())
+			throw new VirusNotFound();
+		return *it;
+	}
+	void create(id_type const & id, id_type const & parent_id);
+	void create(id_type const & id, std::vector<id_type> const & parent_ids);
+	void connect(id_type const & child_id, id_type const & parent_id);
+	void remove(id_type const & id);
 
 private:
 	// struktura na przechowywanie wierzcholkow grafu genealogii -> moze klasa z publicznymi getterami setteram?
